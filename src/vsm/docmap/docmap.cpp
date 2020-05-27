615c003cabc70e6672339d8797ce25136f330f64
ryanYtan <42381062+ryanYtan@users.noreply.github.com>
2020-05-28 03:06:58 +0800

    redo docmap module

--- src/vsm/docmap/docmap.cpp
+++ src/vsm/docmap/docmap.cpp
@@ -6,27 +6,34 @@
 
 namespace vsm
 {
-    const std::string DocumentMap::DELIM_DOCMAP_L1 = "|";
-    const std::string DocumentMap::DELIM_DOCMAP_L2_BEG = "START";
-    const std::string DocumentMap::DELIM_DOCMAP_L2_END = "END";
+    DocumentMap::DocumentMap()
+        : _dmap(DMap())
+    {}
 
-    DocumentMap::DocumentMap() : _dmap(DMap()) {}
-
-    DocumentMap::DocumentMap(DMap dmap) : _dmap(dmap) {}
+    DocumentMap::DocumentMap(DMap dmap)
+        : _dmap(dmap)
+    {}
 
     unsigned int DocumentMap::corpus_size() const noexcept
     {
         return _dmap.size();
     }
 
-    double DocumentMap::vector_length(DocId docid) const noexcept
+    double DocumentMap::vector_length(DocId docid) const
     {
         auto values = util::values(*_dmap.at(docid));
-        auto squared = util::map(std::vector<Count>(values.begin(), values.end()),
-                                 [](int i) { return (double)i * (double)i; });
-        double sum = util::reduce(squared, 0.0,
-                                  [](double x, double y) { return x + y; });
+        auto as_vector = std::vector<Count>(values.begin(), values.end());
+        auto squared = util::map(as_vector, [](int i) { return (double)i * i; });
+        double sum = util::reduce(squared, 0.0, [](double x, double y) {
+            return x + y;
+        });
         return sqrt(sum);
+    }
+
+    Count DocumentMap::of(DocId docid, Term term) const
+    {
+        auto ptr = _dmap.at(docid);
+        return (*ptr).at(term);
     }
 
     void DocumentMap::insert(DocId docid, Term term)
@@ -40,44 +47,12 @@ namespace vsm
         }
     }
 
-    DocumentMap DocumentMap::from_stream(std::istream& in)
+    DocumentMap::VectorPair DocumentMap::items() const
     {
-        DMap dmap;
-
-        bool inside = false;
-        std::string docid;
-
-        for (std::string line; std::getline(in, line);) {
-            line = util::trim(line);
-            if (line == DELIM_DOCMAP_L2_BEG) {
-                inside = true;
-            } else if (line == DELIM_DOCMAP_L2_END) {
-                inside = false;
-            } else if (inside) {
-                auto elements = util::split(line, DELIM_DOCMAP_L1);
-                auto term = elements[0];
-                auto count = static_cast<unsigned int>(std::stoi(elements[1]));
-                (*dmap[docid])[term] = count;
-            } else {
-                docid = line;
-                auto ptr = std::make_shared<TermCountMap>();
-                dmap[docid] = ptr;
-            }
+        DocumentMap::VectorPair ret;
+        for (const auto& [docid, ptr] : _dmap) {
+            ret.push_back(std::make_pair(docid, ptr));
         }
-
-        return DocumentMap(dmap);
+        return ret;
     }
-
-    void DocumentMap::writetofile(std::ostream& out)
-    {
-        for (const auto& [docid, termcountmap] : _dmap) {
-            out << docid << "\n";
-            out << DELIM_DOCMAP_L2_BEG << "\n";
-            for (const auto& [term, count] : *termcountmap) {
-                out << "\t" << term << DELIM_DOCMAP_L1 << count << "\n";
-            }
-            out << DELIM_DOCMAP_L2_END << "\n";
-        }
-    }
-
 }
--- src/vsm/docmap/docmap.h
+++ src/vsm/docmap/docmap.h
@@ -1,10 +1,8 @@
 #ifndef VSM_DOCMAP_DOCMAP_H_INCLUDED
 #define VSM_DOCMAP_DOCMAP_H_INCLUDED
 #include "vsm/vsm_common.h"
-#include <istream>
 #include <map>
 #include <memory>
-#include <ostream>
 
 namespace vsm
 {
@@ -14,20 +12,16 @@ namespace vsm
         using TermCountMap = std::map<Term, Count>;
         using TermCountMapPtr = std::shared_ptr<TermCountMap>;
         using DMap = std::map<DocId, TermCountMapPtr>;
-
-        static const std::string DELIM_DOCMAP_L1;
-        static const std::string DELIM_DOCMAP_L2_BEG;
-        static const std::string DELIM_DOCMAP_L2_END;
+        using VectorPair = std::vector<std::pair<DocId,TermCountMapPtr>>;
 
         DocumentMap();
-        DocumentMap(DMap);
+        DocumentMap(DMap dmap);
 
         unsigned int corpus_size() const noexcept;
-        double vector_length(DocId docid) const noexcept;
+        double vector_length(DocId docid) const;
+        Count of(DocId docid, Term term) const;
         void insert(DocId docid, Term term);
-
-        static DocumentMap from_stream(std::istream& in);
-        void writetofile(std::ostream& out);
+        VectorPair items() const;
 
     private:
         DMap _dmap;
